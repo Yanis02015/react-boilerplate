4b078712be5194c2c0c6cf92c1e48c876dc58a98
Yanis <yaniscrab@gmail.com>
2024-03-27 17:51:57 +0100

    importing global css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./configurations/react-query.ts";
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
